a4f191c7e707624755a2e7a668049eaed1682691
Kolen Cheung <christian.kolen@gmail.com>
2025-05-06 22:51:03 +0100

    Create generate.sh

---
+++ completion/generate.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-"${HOME}/.config"}"
+ZDOTDIR="${ZDOTDIR:-"${XDG_CONFIG_HOME}/zsh"}"
+_ZSH_DIR="${ZDOTDIR}/functions"
+
+XDG_DATA_HOME="${XDG_DATA_HOME:-"${HOME}/.local/share"}"
+BASH_COMPLETION_USER_DIR="${BASH_COMPLETION_USER_DIR:-"${XDG_DATA_HOME}/bash-completion"}"
+_BASH_DIR="${BASH_COMPLETION_USER_DIR}/completions"
+
+mkdir -p "${_BASH_DIR}"
+mkdir -p "${_ZSH_DIR}"
+
+bat --completion bash > "${_BASH_DIR}/_bat"
+bat --completion zsh > "${_ZSH_DIR}/_bat"
+gh completion -s bash > "${_BASH_DIR}/_gh"
+gh completion -s zsh > "${_ZSH_DIR}/_gh"
+pandoc --bash-completion > "${_BASH_DIR}/_pandoc"
+pixi completion --shell bash > "${_BASH_DIR}/_pixi"
+pixi completion --shell zsh > "${_ZSH_DIR}/_pixi"
+starship completions bash > "${_BASH_DIR}/_starship"
+starship completions zsh > "${_ZSH_DIR}/_starship"
+zellij setup --generate-completion bash > "${_BASH_DIR}/_zellij"
+zellij setup --generate-completion zsh > "${_ZSH_DIR}/_zellij"
