0f163634999db3796044224a4a148ea4f45f93b8
Kolen Cheung <christian.kolen@gmail.com>
2025-05-15 19:31:59 +0100

    fix missing newline when COLUMNS unset

--- install/src/lib/util/helpers.sh
+++ install/src/lib/util/helpers.sh
@@ -1,7 +1,16 @@
 print_double_line() {
-    eval printf %.0s= '{1..'"${COLUMNS:-80}"\}
+    if [[ -n ${COLUMNS} ]]; then
+        eval printf %.0s= '{1..'"${COLUMNS}"\}
+    else
+        echo '================================================================================'
+    fi
 }
 
 print_line() {
     eval printf %.0s- '{1..'"${COLUMNS:-80}"\}
+    if [[ -n ${COLUMNS} ]]; then
+        eval printf %.0s- '{1..'"${COLUMNS}"\}
+    else
+        echo '--------------------------------------------------------------------------------'
+    fi
 }
