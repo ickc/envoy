8ba7c76b9b3163a0f60ffbd3a19c5ae20b3e304d
Kolen Cheung <christian.kolen@gmail.com>
2025-05-15 19:40:35 +0100

    make all

--- install/bootstrap.sh
+++ install/bootstrap.sh
@@ -4,47 +4,6 @@ set -eo pipefail
 
 __OPT_ROOT="${__OPT_ROOT:-"${HOME}/.local"}"
 MAMBA_ROOT_PREFIX="${MAMBA_ROOT_PREFIX:-"${HOME}/.miniforge3"}"
-# https://unix.stackexchange.com/a/84980/192799
-DOWNLOADDIR="$(mktemp -d 2> /dev/null || mktemp -d -t 'miniforge3')"
-
-# shellcheck disable=SC2312
-read -r __OSTYPE __ARCH <<< "$(uname -sm)"
-
-mamba_install() {
-    case "${__OSTYPE}-${__ARCH}" in
-        Darwin-arm64) ;;
-        Darwin-x86_64) ;;
-        Linux-x86_64) ;;
-        Linux-aarch64) ;;
-        Linux-ppc64le) ;;
-        *) exit 1 ;;
-    esac
-    # https://github.com/conda-forge/miniforge
-    downloadUrl="https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-${__OSTYPE}-${__ARCH}.sh"
-
-    print_double_line
-    echo Downloading to temp dir "${DOWNLOADDIR}"
-    cd "${DOWNLOADDIR}" || exit 1
-    curl -L "${downloadUrl}" -o Miniforge3.sh
-    chmod +x Miniforge3.sh
-
-    print_double_line
-    if [[ -f "${MAMBA_ROOT_PREFIX}/etc/profile.d/conda.sh" ]]; then
-        echo Updating mamba...
-        ./Miniforge3.sh -ubsp "${MAMBA_ROOT_PREFIX}"
-    else
-        echo Installing mamba...
-        ./Miniforge3.sh -fbsp "${MAMBA_ROOT_PREFIX}"
-    fi
-
-    print_line
-    echo Removing temp dir "${DOWNLOADDIR}"
-    rm -rf "${DOWNLOADDIR}"
-}
-
-mamba_uninstall() {
-    rm -rf "${MAMBA_ROOT_PREFIX}"
-}
 # git 2.3.0 or later is required
 export GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
 
@@ -121,6 +80,174 @@ ssh_dir_install() {
     mv ~/.ssh/id_ed25519.pub ~/ssh-dir
     rm -rf ~/.ssh
     mv ~/ssh-dir ~/.ssh
+}
+BINDIR="${__OPT_ROOT}/bin"
+
+# shellcheck disable=SC2312
+read -r __OSTYPE __ARCH <<< "$(uname -sm)"
+
+code_install() {
+    case "${__OSTYPE}-${__ARCH}" in
+        "Linux-x86_64")
+            url="https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-x64"
+            ;;
+        "Linux-armv7l")
+            url="https://code.visualstudio.com/sha/download?build=stable&os=cli-linux-armhf"
+            ;;
+        "Linux-aarch64")
+            url="https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-arm64"
+            ;;
+        "Darwin-x86_64")
+            url="https://code.visualstudio.com/sha/download?build=stable&os=cli-darwin-x64"
+            ;;
+        "Darwin-arm64")
+            url="https://code.visualstudio.com/sha/download?build=stable&os=cli-darwin-arm64"
+            ;;
+        *)
+            echo "Unsupported OS or architecture"
+            exit 1
+            ;;
+    esac
+
+    case "${__OSTYPE}" in
+        Darwin)
+            if command -v curl > /dev/null; then
+                curl -L "${url}" -o vscode_cli.zip
+            elif command -v wget > /dev/null; then
+                wget "${url}" -O vscode_cli.zip
+            fi
+            unzip vscode_cli.zip
+            rm vscode_cli.zip
+            ;;
+        Linux)
+            if command -v curl > /dev/null; then
+                # shellcheck disable=SC2312
+                curl -fL "${url}" | tar -xz
+            elif command -v wget > /dev/null; then
+                # shellcheck disable=SC2312
+                wget -O - "${url}" | tar -xz
+            fi
+            ;;
+        *) ;;
+    esac
+
+    mkdir -p "${BINDIR}"
+    mv code "${BINDIR}"
+}
+
+code_uninstall() {
+    rm -rf "${BINDIR}/code"
+}
+# https://unix.stackexchange.com/a/84980/192799
+DOWNLOADDIR="$(mktemp -d 2> /dev/null || mktemp -d -t 'miniforge3')"
+
+# shellcheck disable=SC2312
+read -r __OSTYPE __ARCH <<< "$(uname -sm)"
+
+mamba_install() {
+    case "${__OSTYPE}-${__ARCH}" in
+        Darwin-arm64) ;;
+        Darwin-x86_64) ;;
+        Linux-x86_64) ;;
+        Linux-aarch64) ;;
+        Linux-ppc64le) ;;
+        *) exit 1 ;;
+    esac
+    # https://github.com/conda-forge/miniforge
+    downloadUrl="https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-${__OSTYPE}-${__ARCH}.sh"
+
+    print_double_line
+    echo Downloading to temp dir "${DOWNLOADDIR}"
+    cd "${DOWNLOADDIR}" || exit 1
+    curl -L "${downloadUrl}" -o Miniforge3.sh
+    chmod +x Miniforge3.sh
+
+    print_double_line
+    if [[ -f "${MAMBA_ROOT_PREFIX}/etc/profile.d/conda.sh" ]]; then
+        echo Updating mamba...
+        ./Miniforge3.sh -ubsp "${MAMBA_ROOT_PREFIX}"
+    else
+        echo Installing mamba...
+        ./Miniforge3.sh -fbsp "${MAMBA_ROOT_PREFIX}"
+    fi
+
+    print_line
+    echo Removing temp dir "${DOWNLOADDIR}"
+    rm -rf "${DOWNLOADDIR}"
+}
+
+mamba_uninstall() {
+    rm -rf "${MAMBA_ROOT_PREFIX}"
+}
+NAME="${NAME:-system}"
+
+PREFIX="${__OPT_ROOT}/${NAME}"
+
+# shellcheck disable=SC2312
+read -r __OSTYPE __ARCH <<< "$(uname -sm)"
+
+mamba_env_install() {
+    case "${__OSTYPE}-${__ARCH}" in
+        Darwin-arm64) CONDA_UNAME=osx-arm64 ;;
+        Darwin-x86_64) CONDA_UNAME=osx-64 ;;
+        Linux-x86_64) CONDA_UNAME=linux-64 ;;
+        Linux-aarch64) CONDA_UNAME=linux-aarch64 ;;
+        Linux-ppc64le) CONDA_UNAME=linux-ppc64le ;;
+        *) exit 1 ;;
+    esac
+    file="conda/${NAME}_${CONDA_UNAME}.yml"
+
+    if [[ -d ${PREFIX} ]]; then
+        mamba env update -f "${file}" -p "${PREFIX}" -y --prune
+    else
+        mamba env create -f "${file}" -p "${PREFIX}" -y
+    fi
+}
+
+mamba_env_uninstall() {
+    rm -rf "${PREFIX}"
+}
+VERSION=1.0.4
+BINDIR="${__OPT_ROOT}/bin"
+
+# shellcheck disable=SC2312
+read -r __OSTYPE __ARCH <<< "$(uname -sm)"
+
+sman_install() {
+    case "${__OSTYPE}-${__ARCH}" in
+        Darwin-arm64) GO_UNAME=darwin-arm64 ;;
+        Darwin-x86_64) GO_UNAME=darwin-amd64 ;;
+        Linux-x86_64) GO_UNAME=linux-amd64 ;;
+        Linux-aarch64) GO_UNAME=linux-arm64 ;;
+        Linux-ppc64le) GO_UNAME=linux-ppc64le ;;
+        FreeBSD-amd64) GO_UNAME=freebsd-amd64 ;;
+        *) exit 1 ;;
+    esac
+    filename="sman-${GO_UNAME}-v${VERSION}"
+    url="https://github.com/ickc/sman/releases/download/v${VERSION}/${filename}.tgz"
+
+    if command -v curl > /dev/null; then
+        # shellcheck disable=SC2312
+        curl -fL "${url}" | tar -xz
+    elif command -v wget > /dev/null; then
+        # shellcheck disable=SC2312
+        wget -O - "${url}" | tar -xz
+    fi
+    mkdir -p "${BINDIR}"
+    mv "${filename}" "${BINDIR}/sman"
+}
+
+sman_uninstall() {
+    rm -f "${BINDIR}/sman"
+}
+ZIM_HOME="${ZIM_HOME:-${HOME}/.zim}"
+
+zim_install() {
+    curl -fsSL --create-dirs -o "${ZIM_HOME}/zimfw.zsh" https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
+}
+
+zim_uninstall() {
+    rm -rf "${ZIM_HOME}"
 }
 
 main() {
