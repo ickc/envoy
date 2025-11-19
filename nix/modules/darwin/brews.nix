dc7c4aaaa25a30523d6e32bc3c1eca2edb52fa44
Kolen Cheung <christian.kolen@gmail.com>
2025-11-19 11:08:31 +0000

    remove comments

--- nix/modules/darwin/brews.nix
+++ nix/modules/darwin/brews.nix
@@ -1,6 +1,3 @@
-# nix-darwin/brews.nix
-# Declarative list of Homebrew formulae for nix-darwin `homebrew.brews`.
-# This file is imported from `flake.nix` (e.g. `brews = import ./brews.nix;`).
 [
   # CLI for ImageOptim, ImageAlpha and JPEGmini
   "imageoptim-cli"
